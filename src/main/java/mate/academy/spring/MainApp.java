05e764997d8efd4d6644d3e21c7998470a7082c3
roman_minevich <283389dv3qsh>
2019-10-31 12:30:49 +0200

    Removed unnecessary exception throwing from MainApp class

--- src/main/java/mate/academy/spring/MainApp.java
+++ src/main/java/mate/academy/spring/MainApp.java
@@ -1,11 +1,10 @@
 package mate.academy.spring;
 
-import java.sql.SQLException;
 import mate.academy.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
